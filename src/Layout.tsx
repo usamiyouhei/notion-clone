66f2029401f07d78893a8cdc5c8fc6cd6616cb61
youhei usami <165367050+youhei-usami@users.noreply.github.com>
2025-06-28 09:40:49 +0400

    6/28

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,15 +2,34 @@ import { Navigate, Outlet } from 'react-router-dom';
 import SideBar from './components/SideBar';
 import { SearchModal } from './components/SearchModal';
 import { useCurrentUserStore } from "./modules/auth/current-user.state";
+import { useNoteStore } from "./modules/notes/note.state";
+import { useState, useEffect } from "react";
+import { noteRepository } from "./modules/notes/note.repository";
 
 const Layout = () => {
-   const { currentUser } = useCurrentUserStore();
+    const { currentUser } = useCurrentUserStore();
+    const noteStore = useNoteStore();
+    const [isLoding, setIsLoading] = useState(false);
 
-   if(currentUser == null) return <Navigate replace to="/signin" />
+    useEffect(() => {
+      fetchNotes()
+    }, [])
+
+
+    const fetchNotes = async () => {
+    setIsLoading(true);
+    const notes = await noteRepository.find(currentUser!.id)
+    if(notes == null) return;
+    noteStore.set(notes);
+    setIsLoading(false);
+    }
+
+
+    if(currentUser == null) return <Navigate replace to="/signin" />
 
   return (
     <div className="h-full flex">
-      <SideBar onSearchButtonClicked={() => {}} />
+      {!isLoding && <SideBar onSearchButtonClicked={() => {}} />}
       <main className="flex-1 h-full overflow-y-auto">
         <Outlet />
         <SearchModal
