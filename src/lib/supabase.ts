1f914584bbad2b70e96ec3c82ebbfeba227f8bde
youhei usami <165367050+youhei-usami@users.noreply.github.com>
2025-06-29 10:21:06 +0400

    6/29

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "../../database.types";
 
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_API_KEY,
 
 );
+
+// const hoge = supabase.from('notes').select();
