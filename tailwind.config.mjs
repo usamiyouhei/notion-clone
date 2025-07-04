7de8717db9b5acbf80f06a904c1a8bed8939cc37
youhei usami <165367050+youhei-usami@users.noreply.github.com>
2025-07-04 17:08:37 +0400

    7/4

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,10 @@
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  plugins: [react(), require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
   theme: {
   	extend: {
   		colors: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import path from 'path';
-import tailwindcss from 'tailwindcss';
+// import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
